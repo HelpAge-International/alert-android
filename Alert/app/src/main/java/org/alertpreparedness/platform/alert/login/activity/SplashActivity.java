ee8f94fda2cd39b68a1f5926c66af850b0df7202
Faiz Mohideen <faiz@rolleragency.co.uk>
2017-11-06 13:11:00 +0000

    Login UI updated - Fixed package name

--- Alert/app/src/main/java/org/alertpreparedness/platform/alert/login/activity/SplashActivity.java
+++ Alert/app/src/main/java/org/alertpreparedness/platform/alert/login/activity/SplashActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.alertpreparedness.platform.alert.dashboard.activity.HomeScreen;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashActivity.this, LoginScreen.class));
+        startActivity(new Intent(SplashActivity.this, HomeScreen.class));
         finish();
     }
 }
