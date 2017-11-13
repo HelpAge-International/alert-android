93167a9a1a6ddf0b98bf8824d93998ee94fefaec
Faiz Mohideen <faiz@rolleragency.co.uk>
2017-11-13 17:41:15 +0000

    Dashboard

---
+++ Alert/app/src/main/java/org/alertpreparedness/platform/alert/dashboard/adapter/TaskAdapter.java
@@ -0,0 +1,8 @@
+package org.alertpreparedness.platform.alert.dashboard.adapter;
+
+/**
+ * Created by faizmohideen on 13/11/2017.
+ */
+
+public class TaskAdapter {
+}
---
+++ Alert/app/src/main/java/org/alertpreparedness/platform/alert/model/Tasks.java
@@ -0,0 +1,8 @@
+package org.alertpreparedness.platform.alert.model;
+
+/**
+ * Created by faizmohideen on 13/11/2017.
+ */
+
+public class Tasks {
+}
