8b8408e7b53a25bcafc33230ab6cd326768304cd
Sebastian Ghetu <sebastian@rolleragency.co.uk>
2018-02-08 17:41:22 +0000

    Created menu, WIP most of the logic

--- Alert/app/src/main/java/org/alertpreparedness/platform/alert/min_preparedness/fragment/ActionUnassignedFragment.java
+++ Alert/app/src/main/java/org/alertpreparedness/platform/alert/min_preparedness/fragment/ActionUnassignedFragment.java
@@ -1,46 +1,43 @@
 package org.alertpreparedness.platform.alert.min_preparedness.fragment;
 
-import android.app.Fragment;
-import android.os.Build;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
-import org.alertpreparedness.platform.alert.MainDrawer;
 import org.alertpreparedness.platform.alert.R;
 import org.alertpreparedness.platform.alert.dagger.DependencyInjector;
 import org.alertpreparedness.platform.alert.dagger.annotation.BaseActionRef;
-import org.alertpreparedness.platform.alert.helper.DateHelper;
 import org.alertpreparedness.platform.alert.min_preparedness.adapter.ActionAdapter;
 import org.alertpreparedness.platform.alert.min_preparedness.adapter.PreparednessAdapter;
-import org.alertpreparedness.platform.alert.min_preparedness.model.Action;
-import org.alertpreparedness.platform.alert.min_preparedness.model.DataModel;
-import org.alertpreparedness.platform.alert.model.User;
 import org.alertpreparedness.platform.alert.utils.Constants;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import ru.whalemare.sheetmenu.SheetMenu;
 
 /**
  * Created by faizmohideen on 21/12/2017.
@@ -60,7 +57,6 @@ public class ActionUnassignedFragment extends BaseUnassignedFragment {
     @Inject
     @BaseActionRef
     DatabaseReference baseActionRef;
-
 
     @BindView(R.id.tvNoAction)
     TextView txtNoAction;
@@ -137,13 +133,57 @@ public class ActionUnassignedFragment extends BaseUnassignedFragment {
 
     @Override
     public void onActionItemSelected(int pos, String key, String userTypeID) {
+        SheetMenu.with(getContext()).setMenu(R.menu.menu_unassigned).setClick(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.update_date:
+                    showDatePicker(key);
+                    break;
+                case R.id.assign_action:
+                    assignAction();
+                    break;
+                case R.id.action_notes:
+                    break;
+                case R.id.attachments:
+                    break;
+            }
+        }).show();
+
         Snackbar.make(getActivity().findViewById(R.id.cl_in_progress), "Currently under development!", Snackbar.LENGTH_LONG).show();
+    }
+
+    private void assignAction() {
+        
+    }
+
+    private void showDatePicker(String key) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog pickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                String givenDateString = i2 + " " + i1 + " " + i + " 23:59:00";//due the end of the day.
+                SimpleDateFormat sdf = new SimpleDateFormat("dd mm yyyy HH:mm:ss", Locale.getDefault());
+                try {
+                    Date mDate = sdf.parse(givenDateString);
+                    long timeInMilliseconds = mDate.getTime();
+                    long millis = System.currentTimeMillis();
+
+                    dbActionRef.child(key).child("dueDate").setValue(timeInMilliseconds);//save due date in milliSec.
+                    dbActionRef.child(key).child("updatedAt").setValue(millis);
+
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, year, month, day);
+        pickerDialog.show();
     }
 
     @Override
     protected TextView getNoActionView() {
         return txtNoAction;
     }
-
-
 }
