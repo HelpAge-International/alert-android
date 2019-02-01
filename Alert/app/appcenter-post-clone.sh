c25265ccfb2b8384355f554fb06aa83ffaf7adf2
Elliot <ells1231@gmail.com>
2019-02-01 13:14:08 +0000

    Fixed Build script, updated to canary 3

--- Alert/app/appcenter-post-clone.sh
+++ Alert/app/appcenter-post-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 version=`git describe`
-sed -i '.bak' 's/versionName ".*"/versionName "test"/g' build.gradle
+echo "Version $version"
+sed -i '.bak' "s/versionName \".*\"/versionName \"$version\"/g" build.gradle
